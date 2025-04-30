995ac9ecc961277ce3583a23b84e43e3a9a787f1
Andrew Kane <andrew@ankane.org>
2025-04-30 14:13:14 -0700

    Added GC stress testing [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,20 @@
 require "bundler/setup"
 Bundler.require(:default)
 require "minitest/autorun"
+
+class Minitest::Test
+  def setup
+    # autoload before GC.stress
+    RDKit::FFI if stress?
+
+    GC.stress = true if stress?
+  end
+
+  def teardown
+    GC.stress = false if stress?
+  end
+
+  def stress?
+    ENV["STRESS"]
+  end
+end
